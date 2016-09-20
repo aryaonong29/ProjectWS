44196e74e8b8a9c978e128720e6b48356b5d5c85
arianasp <aryaonong29@gmail.com>
2016-09-20 11:40:35 +0700

    layout

--- app/src/main/java/com/arianasp/projectws/MainActivity.java
+++ app/src/main/java/com/arianasp/projectws/MainActivity.java
@@ -1,13 +1,26 @@
 package com.arianasp.projectws;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
         setContentView(R.layout.activity_main);
     }
 }
